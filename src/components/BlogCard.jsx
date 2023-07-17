ffc5d57a686b5157cde579e3674bdbb27f150ca9
mdalmamunit427 <mdalmamunit427@gmail.com>
2023-07-17 21:59:39 +0600

    dynamic blog page

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { Card } from "flowbite-react";
+import Link from "next/link";
 // import Image from "next/image";
 
 const BlogCard = ({ blog }) => {
-  const { title, description, image } = blog;
+  const { title, description, image, id } = blog;
   // console.log(blog);
   return (
     <Card
@@ -19,7 +20,7 @@ const BlogCard = ({ blog }) => {
       <p className="font-normal text-gray-700 dark:text-gray-400">
         {description}
       </p>
-      <button className="btn hover:bg-indigo-700 hover:text-white">Read More</button>
+      <Link href={`/blogs/${id}`}><button className="btn hover:bg-indigo-700 hover:text-white">Read More</button></Link>
     </Card>
   );
 };
